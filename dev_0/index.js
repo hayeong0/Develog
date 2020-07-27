636bc2c23b7b5af3b58e9dff39c52f39f1323245
chy0428 <21800758@handong.edu>
2020-07-27 21:43:12 +0900

    Update index.js

--- dev_0/index.js
+++ dev_0/index.js
@@ -2,5 +2,14 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const mongoose = require('mongoose')
+mongoose.connect('mongodb+srv://hayeong:<password>@ha0.hnyx8.mongodb.net/<dbname>?retryWrites=true&w=majority', {
+    useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
+}).then(() => console.log('MongoDB connected...'))
+.catch(error => console.log(error))
+
 app.get('/', (req, res) => res.send('Develog!'))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+
+
