56a62ca4564c6f52f8887356449b468644ce2fe3
KoEonYack <21300035@handong.edu>
2020-07-28 22:29:23 +0900

    Refactor port config

--- dev_0/index.js
+++ dev_0/index.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const app = express()
-const port = 3000
 const bodyParser = require('body-parser')
 const { User } = require('./models/User')
 
@@ -34,7 +33,6 @@ app.post('/register', (req, res) => {
 
 
 app.get('/', (req, res) => res.send('Develog!'))
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
-
-
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`))
