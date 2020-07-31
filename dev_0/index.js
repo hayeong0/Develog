f23b348e2726ff18607354b1daaa0dfb250959d7
chy0428 <21800758@handong.edu>
2020-07-31 15:01:28 +0900

    Update

--- dev_0/index.js
+++ dev_0/index.js
@@ -1,8 +1,9 @@
-const express = require('express')
-const app = express()
-const bodyParser = require('body-parser')
-const { User } = require('./models/User')
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 
+const { User } = require('./models/User');
 // DB Config
 const db = require('./config/keys');
 
@@ -10,6 +11,7 @@ const db = require('./config/keys');
 app.use(bodyParser.urlencoded({extended: true}));
 // applicaion/json
 app.use(bodyParser.json());
+app.use(cookieParser());
 
 // Connect to Mongo
 const mongoose = require('mongoose');
@@ -31,6 +33,38 @@ app.post('/register', (req, res) => {
             success: true
         })
     })
+})
+
+app.post('/login', (req, res) => {
+    // 요청된 이메일이 데이터베이스에 있는지 찾기
+    User.findOne({ email: req.body.email }, (err, user) => {
+        if(!user) {
+            return res.json({
+                loginSuccess: false,
+                message: "User not found."
+            })
+        }
+        // 비밀번호 일치 여부 확인
+        user.comparePassword(req.body.password, (err, isMatch) => {
+            if(!isMatch)
+            return res.json({ 
+                loginSuccess: false, 
+                message: "Passwords do not match."
+            })
+            
+            // user를 위한 token 생성
+            user.generateToken((err, user) => {
+                if(err) return res.status(400).send(err);
+
+                // token 저장 (쿠키)
+                res.cookie("x_auth", user.token)
+                .status(200)
+                .json({
+                    loginSuccess: true, 
+                    userID: user._id})
+            })
+        })
+    })    
 })
 
 const PORT = process.env.PORT || 3000
